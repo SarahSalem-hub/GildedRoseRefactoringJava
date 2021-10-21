86f48fd67a6f302d4f53366d8ac7bbabab1a9e06
sarahsalem-hub <sarsarine01@gmail.com>
2021-10-21 17:09:01 +0300

    All tested Tests are added

--- GildedRose-Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ GildedRose-Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,6 +1,7 @@
 package com.gildedrose;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -14,6 +15,11 @@ public class GildedRoseTest {
 
         app.updateQuality();
         return app;
+    }
+    @Test
+    public void TestTheTruth()
+    {
+        assertTrue(true);
     }
 
     @Test
@@ -45,5 +51,42 @@ public class GildedRoseTest {
 
         //assert sellin
         assertEquals(9, app.items[0].sellIn);
+    }
+    @Test
+    public void TheOlderItGets()
+    {
+        //arrange
+        GildedRose app = CreateAndUpdate("Aged Brie", 1, 0);
+
+        //assert sellin
+        assertEquals(0, app.items[0].sellIn);
+
+        //assert quality
+        assertEquals(1, app.items[0].quality);
+
+    }
+    @Test
+    public void QualityNeverIncreaseThanFifty()
+    {
+        //arrange
+        GildedRose app = CreateAndUpdate("Aged Brie", 0, 49);
+
+        //assert quality
+        assertEquals(50, app.items[0].quality);
+
+
+    }
+
+    @Test
+    public void SulfurasLegendaryItem()
+    {
+        //arrange
+        GildedRose app = CreateAndUpdate("Sulfuras, Hand of Ragnaros",0, 80);
+
+        //assert quality
+        assertEquals(80, app.items[0].quality);
+
+        //assert sellin
+        assertEquals(0, app.items[0].sellIn);
     }
 }
